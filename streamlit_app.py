334e29fdf30a55dc4e36716726e27e03f48b3639
Deepak Ranjan <34955340+shivaydeep@users.noreply.github.com>
2024-08-10 15:05:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,9 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object
-conn = st.connection("gsheets", type=GSheetsConnection)
-google_sheets_table = conn.read()
-dataframe = pd.DataFrame(google_sheets_table)  # Convert Google Sheets table into a pandas dataframe.
+# Load data from the Excel file
+file_path = '/mnt/data/TOP 5 QR Code generator & Feature comparison.xlsx'
+dataframe = pd.read_excel(file_path)
 
-# Streamlit content to display the dataframe with images
-for index, row in dataframe.iterrows():
-    # Assuming the image URL is in a column named "Image"
-    st.image(row['Image'], caption=row['Name'])  # Adjust 'Image' and 'Name' to the actual column names in your sheet
-    st.write(row.drop('Image'))  # Display other data except for the Image URL
-
-# Alternatively, you can display the entire dataframe as well
-st.write(dataframe.columns)
+# Streamlit content to display the dataframe
+st.write(dataframe)
